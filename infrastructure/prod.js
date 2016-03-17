a66023264860334b67f10f7bef6e715aadbb89b1
Damian Rawski <damian.rawski@schibsted.pl>
2016-03-17 11:17:57 +0100

    Refactored infrastructure

--- infrastructure/prod.js
+++ infrastructure/prod.js
@@ -1,29 +1,9 @@
-var heroin = require('heroin-js');
-
-var configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
+var base = require('./base');
+var _ = require('lodash');
 
 var prod = {
-    name: 'nodejs-book-inventory',
-    region: 'eu',
-    maintenance: false,
-    stack: 'cedar-14',
-    config_vars: { MONGOLAB_URI: process.env.MONGOLAB_URI },
-    addons: {},
-    collaborators:
-        [ 'katarzyna.wlodarska@schibsted.pl',
-            'damian.rawski@schibsted.pl',
-            'marcin.juszkiewicz@schibsted.pl',
-            'konrad.turczynski@schibsted.pl' ],
-            features:
-    { 'runtime-dyno-metadata': { enabled: false },
-        'log-runtime-metrics': { enabled: false },
-        'http-session-affinity': { enabled: false },
-        preboot: { enabled: false },
-        'http-shard-header': { enabled: false },
-        'http-end-to-end-continue': { enabled: false } },
-    formation: [ { process: 'web', quantity: 1, size: 'Free' } ],
-        log_drains: [],
-    domains: [ 'nodejs-book-inventory.herokuapp.com' ]
+    name: 'nodejs-book-inventory'
 };
 
-configurator(prod);
+base.configurator(_.merge({}, base.settings, prod));
+
